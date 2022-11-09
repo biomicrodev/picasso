f951f8bbc2d37aed3e3ae591b7f81ad0aca90752
Sebastian Ahn <wooahns@gmail.com>
2022-11-09 16:32:07 -0500

    Clean up algorithm, stopping condition, outputs, variable names.

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ A simple python implementation of the below paper for the linear unmixing of spe
 
 [PICASSO allows ultra-multiplexed fluorescence imaging of spatially overlapping proteins without reference spectra measurements](https://www.nature.com/articles/s41467-022-30168-z)
 
+## Notes
+Images to be unmixed should be smoothed or downsampled to prevent poor performance. Right now the inputs to the functions are assuming CYX format, with the unmixing occurring along the C axis, but this can be extended to specify an arbitrary axis.
--- src/picasso/unmixing.py
+++ src/picasso/unmixing.py
@@ -1,19 +1,20 @@
 from __future__ import annotations
 
 import math
-from typing import Callable
+import sys
 
 import numpy as np
 import numpy.typing as npt
 from fast_histogram import histogram2d, histogram1d
 from scipy.optimize import OptimizeResult, minimize
+from skimage import img_as_float
 from tqdm import tqdm
 
 
 def shannon_entropy(a: npt.NDArray) -> float:
     """
     This runs very often, so we should do our best to make this fast.
-    See profiling/shannon-entropy.ipynb for why this was chosen
+    See 'profiling/shannon-entropy.ipynb' for why this was chosen
     """
     # seems like flattened arrays are faster, but .flatten() produces a copy so we
     # stick with .ravel()
@@ -31,6 +32,8 @@ def mutual_information(x: npt.NDArray, y: npt.NDArray, *, bins=100) -> float:
     x_range = (x.min(), x.max())
     y_range = (y.min(), y.max())
 
+    # TODO: even though fast-histogram is pretty dang fast, consider boost-histogram?
+    #  measure perf diff
     c_xy = histogram2d(x, y, bins, (x_range, y_range))
     c_x = histogram1d(x, bins, x_range)
     c_y = histogram1d(y, bins, y_range)
@@ -42,16 +45,10 @@ def mutual_information(x: npt.NDArray, y: npt.NDArray, *, bins=100) -> float:
     return h_x + h_y - h_xy
 
 
-def cross_correlation(x: npt.NDArray, y: npt.NDArray) -> float:
-    return (
-        np.dot(x, y).sum() / np.sqrt(np.square(x).sum() * np.square(y).sum())
-    ).item()
-
-
 def regional_mi(x: npt.NDArray, y: npt.NDArray) -> float:
     """
     FIXME not optimized, but doesn't give us better results than regular mi, so not
-     worth it atm
+     worth it at the moment
     """
     x = np.copy(x)
     y = np.copy(y)
@@ -91,8 +88,7 @@ def minimize_mi(x: npt.NDArray, y: npt.NDArray, *, init_alpha=0.0) -> float:
     non_neg_alpha_constraint = dict(
         type="ineq",
         fun=lambda a: a,
-        # catol=1e-3
-        # catol=1e-7,
+        catol=1e-7,
     )
 
     result: OptimizeResult = minimize(
@@ -108,55 +104,58 @@ def compute_unmixing_matrix(
     image: npt.NDArray,
     *,
     max_iters=1_000,
-    step_mult=0.05,
-    atol=1e-3,
-    return_iters=False,
-    constrain_diag=False,
+    step_mult=0.1,
+    constrain_diag=True,
     verbose=False,
-    stop_requested: Callable[[], bool] | None = None,
-) -> npt.NDArray | None:
-    # alpha is the unmixing matrix, but that is too verbose as a variable name
-    if return_iters:
-        alpha_iters = []
-
+    return_iters=False,
+) -> npt.NDArray:
     n_channels = image.shape[0]
-    alpha_cumul = np.eye(n_channels)
-    alpha_cumul_last = np.eye(n_channels)
+    mats = []
+
+    image = img_as_float(image)
+    image_orig = image.copy()
+
+    mat_cumul = np.eye(n_channels)
+    mat_last = np.eye(n_channels)
 
     for _ in tqdm(
-        range(max_iters), disable=not verbose, desc="unmixing iters", total=0
+        range(max_iters),
+        disable=not verbose,
+        desc="Unmixing iterations",
+        total=0,
+        file=sys.stdout,
     ):
-        alpha = np.eye(n_channels)
+        mat = np.eye(n_channels, dtype=float)
 
         for row in range(n_channels):
             for col in range(n_channels):
-                if stop_requested is not None:
-                    if stop_requested():
-                        return
                 if row == col:
                     continue
 
-                alpha[row, col] = -step_mult * minimize_mi(
+                mat[row, col] = -step_mult * minimize_mi(
                     image[col, ...], image[row, ...]
                 )
 
-        # several times faster than np.einsum
-        image = np.tensordot(alpha, image, axes=1)
-
-        # no need for this to be fast
-        alpha_cumul = alpha @ alpha_cumul
-
-        if constrain_diag:
-            for i in range(n_channels):
-                alpha_cumul[i, i] = 1
-        if return_iters:
-            alpha_iters.append(alpha_cumul)
-        if np.allclose(alpha_cumul, alpha_cumul_last, atol=atol):
+        # check this early on
+        if np.allclose(mat, mat_last):
             break
 
-        alpha_cumul_last = alpha_cumul.copy()
+        # update matrix
+        mat_last = mat
+        assert mat_cumul.shape == (n_channels, n_channels)
+        mat_cumul = mat @ mat_cumul
+        if constrain_diag:
+            for i in range(n_channels):
+                mat_cumul[i, i] = 1
+        mats.append(mat_cumul)
+
+        # update the next iteration of image
+        # several times faster than np.einsum
+        assert mat.shape == (n_channels, n_channels)
+        assert image.ndim == 3
+        image = np.tensordot(mat_cumul, image_orig, axes=1)
 
     if return_iters:
-        return np.stack(alpha_iters)
+        return np.stack(mats)
     else:
-        return alpha_cumul
+        return mats[-1]
